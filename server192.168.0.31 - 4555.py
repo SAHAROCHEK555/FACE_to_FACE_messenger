0a79fd3b915d76f0131c570fb287833ce7de3f38
SAHAOCHEK555 <hrenovsana679@gmail.com>
2023-08-17 23:16:58 +0500

    interface

--- server192.168.0.31 - 4555.py
+++ server192.168.0.31 - 4555.py
@@ -1,29 +1,26 @@
 import socket
 import sqlite3
-import random
-max_size = 65535
+import eel
 
 conn = sqlite3.connect('client&serverDB.db', check_same_thread=False)
 cursor = conn.cursor()
+
 def db_table_val(soobzenia: str):
     cursor.execute('INSERT INTO all_mes (soobzenia) VALUES (?)', (soobzenia,))
     conn.commit()
-    
-def cvet(text):
-    print("\033[34m\033[45m{}".format(text))
-
-def listen(host: str = '192.168.0.31', port: int = 4555):
+            
+ 
+def listen(host: str = '192.168.0.31', port: int = 8000):
     soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
     soc.bind((host, port))
-    print(f'Listening at {host}:{port}')
+    print(f'Listening at {host} : {port}')
     
 
     messages = []
     memb = []
     
     while True:
-            msg, addr = soc.recvfrom(max_size)
+            msg, addr = soc.recvfrom(1024)
             
             if addr not in memb:
                 memb.append(addr)
@@ -34,18 +31,23 @@ def listen(host: str = '192.168.0.31', port: int = 4555):
 
                      
             if msg.decode('utf-8').startswith('SIGNUP_TAG:'):
-                name1 = msg.decode('utf-8')[msg.decode('utf-8').index(':')+1:]
+                name1 = addr[1]
                 print(f'Client {name1} joined chat!')
-
+                continue
 
             
-            else:    
-                for member in memb:
-                    if member == addr:
-                        continue
-                    soc.sendto(msg, member)
-                    messages.append(msg.decode('utf-8'))
-                    db_table_val(soobzenia=msg.decode('utf-8'))
-                    
+            print(msg.decode("utf-8")) 
+            
+            for member in memb:
+                if member == addr:
+                    continue
+                
+                soc.sendto(msg, member) 
+                messages.append(msg.decode('utf-8'))
+                db_table_val(soobzenia=msg.decode('utf-8'))
+
+
+    
+
 if __name__ == '__main__':
     listen()
